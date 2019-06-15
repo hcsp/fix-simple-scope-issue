c0547959ba6fb39d547f704306b9ee2735896ab9
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,14 +1,61 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Random;
+import java.util.Stream.IntStream;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void relationTest() {
-        assertEquals("5<10", Main.relation(5, 10));
-        assertEquals("1>-1", Main.relation(1, -1));
-        assertEquals("0=0", Main.relation(0, 0));
+        Assertions.assertEquals("5<10", Main.relation(5, 10));
+        Assertions.assertEquals("1>-1", Main.relation(1, -1));
+        Assertions.assertEquals("0=0", Main.relation(0, 0));
+    }
+
+    @Test
+    public void concurrentInvocationSafetyTest() throws ExecutionException, InterruptedException {
+        CyclicBarrier cb = new CyclicBarrier(100);
+        ExecutorService threads = Executors.newCachedThreadPool();
+
+        List<Future> futures =
+                IntStream.range(0, 100)
+                        .mapToObj(
+                                (i) ->
+                                        threads.submit(
+                                                () -> {
+                                                    Random random = new Random();
+                                                    int a = random.nextInt(100);
+                                                    int b = random.nextInt(100);
+                                                    try {
+                                                        cb.await();
+                                                    } catch (InterruptedException
+                                                            | BrokenBarrierException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                    String relation = Main.relation(a, b);
+                                                    Assertions.assertTrue(
+                                                            relation.contains(
+                                                                    a > b
+                                                                            ? ">"
+                                                                            : (a == b ? "=" : "<")),
+                                                            () ->
+                                                                    String.format(
+                                                                            "Thread: %d, a: %d, b: %d, relation: %s",
+                                                                            Thread.currentThread()
+                                                                                    .getId(),
+                                                                            a,
+                                                                            b,
+                                                                            relation));
+                                                }))
+                        .collect(Collectors.toList());
+        for (Future future : futures) {
+            future.get();
+        }
     }
 }
