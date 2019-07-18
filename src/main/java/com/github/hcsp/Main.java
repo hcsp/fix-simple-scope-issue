2ab6eb86f0d2e6ed326f2c824fb25de126186d63
Wang Peng <947034046@qq.com>
2019-07-18 23:19:44 +0800

    Fixed the compilation error.

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -12,13 +12,11 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
-        if (a > b) {
-            String sign = ">";
-        } else if (a < b) {
-            String sign = "<";
-        } else {
-            String sign = "=";
-        }
+        String sign = a > b
+                ? ">"
+                : a < b
+                ? "<"
+                : "=";
 
         return "" + a + sign + b;
     }
