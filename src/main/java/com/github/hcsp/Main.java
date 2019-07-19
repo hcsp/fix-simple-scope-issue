52c66aaa5367af5d36b8f5cc97557e3a735391e9
gaozhidong <1011284295@qq.com>
2019-07-19 09:47:55 +0800

    作用域

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp;
 
+import jdk.nashorn.internal.runtime.Undefined;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(relation(1, 2));
@@ -12,12 +14,15 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
-        if (a > b) {
-            String sign = ">";
+        String sign;
+//        String sign = null;
+//        String sign = "";
+        if (a > b) {//我在这一行打断点，发现 String sign; sign 并没有被创建，Sting sign = null;和Sting sign = ""; 分别创建了 sign = null ; sign = "";
+            sign = ">";
         } else if (a < b) {
-            String sign = "<";
+            sign = "<";
         } else {
-            String sign = "=";
+            sign = "=";
         }
 
         return "" + a + sign + b;
