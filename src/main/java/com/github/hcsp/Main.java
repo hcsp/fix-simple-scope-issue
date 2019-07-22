76cc9484000f9af79ccb2a58ee62b89778fae0bf
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-22 19:53:37 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -12,14 +12,15 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
+        String sign = "";
         if (a > b) {
-            String sign = ">";
+            sign = ">";
         } else if (a < b) {
-            String sign = "<";
+            sign = "<";
         } else {
-            String sign = "=";
+            sign = "=";
         }
 
-        return "" + a + sign + b;
+        return "a" + sign + "b";
     }
 }
