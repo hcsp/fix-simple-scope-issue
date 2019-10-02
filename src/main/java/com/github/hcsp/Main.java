159409dec39c97c0e6afa90235298b8bc72f6395
mattisonZhao <mattison@yeah.net>
2019-10-02 22:58:49 +0800

    scope issue

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,18 +6,18 @@ public class Main {
         System.out.println(relation(2, 1));
         System.out.println(relation(2, 2));
     }
-
+    public static String sign = "";
     // Fix the compilation error
     // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
         if (a > b) {
-            String sign = ">";
+            sign = ">";
         } else if (a < b) {
-            String sign = "<";
+            sign = "<";
         } else {
-            String sign = "=";
+            sign = "=";
         }
 
         return "" + a + sign + b;
