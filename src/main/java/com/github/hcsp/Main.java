0ffea8e0b91e99c313721d5f9b62b4840dd52fed
ws1603 <18388866683@163.com>
2019-10-08 22:17:22 +0800

    修复了sign的作用域

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,9 +11,9 @@ public class Main {
     // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
-    public static String sign;
 
     public static String relation(int a, int b) {
+        String sign;
         if (a > b) {
             sign = ">";
         } else if (a < b) {
