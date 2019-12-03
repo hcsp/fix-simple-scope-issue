d7f03299f24ac924963b7ee946f490443af345d1
陶思亮 <32303633+tsl1127@users.noreply.github.com>
2019-12-03 21:55:06 +0800

    修改作用域 (#121)
    修改作用域 (#121)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,17 +7,19 @@ public class Main {
         System.out.println(relation(2, 2));
     }
 
+
     // Fix the compilation error
     // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
+        String sign ;
         if (a > b) {
-            String sign = ">";
+            sign = ">";
         } else if (a < b) {
-            String sign = "<";
+             sign = "<";
         } else {
-            String sign = "=";
+             sign = "=";
         }
 
         return "" + a + sign + b;
