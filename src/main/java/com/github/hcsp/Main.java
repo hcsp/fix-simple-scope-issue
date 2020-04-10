cdc34cd28b23ad9debabd5582203a6893645dbb3
yipinchen <haxilama110611@163.com>
2020-04-10 13:30:33 +0800

    作用域

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,6 +1,6 @@
 package com.github.hcsp;
 public class Main {
-    public static String sign;
+//    public static String sign;
     //在这里也可以，反正要大于return，不过鉴于结构清晰，肯定放他自己的函数中
     public static void main(String[] args) {
         System.out.println(relation(1, 2));
@@ -8,7 +8,7 @@ public class Main {
         System.out.println(relation(2, 2));
     }
     public static String relation(int a, int b) {
-//        String sign;   sign在if外面定义就行，要不然return里的sign没用。
+        String sign;
         if (a > b) {
              sign = ">";
         } else if (a < b) {
