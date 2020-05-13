b4b73d924689c6701ba20c935e1e4b77f9cf9a2e
hanchenhao <hanchenhao@vip.qq.com>
2020-05-13 22:12:38 +0800

    task

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,6 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
+    static String sign;
     public static void main(String[] args) {
         System.out.println(relation(1, 2));
         System.out.println(relation(2, 1));
@@ -12,13 +13,9 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
-        if (a > b) {
-            String sign = ">";
-        } else if (a < b) {
-            String sign = "<";
-        } else {
-            String sign = "=";
-        }
+        sign = a > b ? ">" :
+                a < b ? "<" :
+                        "=";
 
         return "" + a + sign + b;
     }
