f52361bfafa3a9c40a33e2e90a8b1ff7276dd4c0
yangbisheng <2606424906@qq.com>
2021-08-04 04:32:42 +0800

    fix the question in the main (#265)
    fix the question in the main (#265)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,6 +2,7 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(relation(1, 2));
         System.out.println(relation(2, 1));
         System.out.println(relation(2, 2));
@@ -12,14 +13,15 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
+        String i;
         if (a > b) {
-            String sign = ">";
+            i = ">";
         } else if (a < b) {
-            String sign = "<";
+            i = "<";
         } else {
-            String sign = "=";
+            i = "=";
         }
 
-        return "" + a + sign + b;
+        return "" + a + i + b;
     }
 }
