73e72fdba1d3b76048d2476796076c9be2819931
Gengooooo <32353324+leoleepsyche@users.noreply.github.com>
2021-09-26 18:37:33 +0100

    Geng (#274)
    Geng (#274)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,10 @@
 package com.github.hcsp;
 
 public class Main {
+
+
     public static void main(String[] args) {
+
         System.out.println(relation(1, 2));
         System.out.println(relation(2, 1));
         System.out.println(relation(2, 2));
@@ -12,12 +15,13 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
+        String sign;
         if (a > b) {
-            String sign = ">";
+             sign = ">";
         } else if (a < b) {
-            String sign = "<";
+             sign = "<";
         } else {
-            String sign = "=";
+             sign = "=";
         }
 
         return "" + a + sign + b;
