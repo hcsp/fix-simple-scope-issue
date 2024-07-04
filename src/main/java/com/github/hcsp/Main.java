dfe5922f30db291ecc9b5727d9c14dbcc6a552b0
buuoltwo <bullnick20@gmail.com>
2024-07-04 23:43:06 +0800

    2

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
-    private static String sign;
+//    private static String sign;
     public static void main(String[] args) {
         System.out.println(relation(1, 2));
         System.out.println(relation(2, 1));
@@ -13,6 +13,7 @@ public class Main {
     // 修复编译错误
     // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
     public static String relation(int a, int b) {
+        String sign;
         if (a > b) {
              sign = ">";
         } else if (a < b) {
