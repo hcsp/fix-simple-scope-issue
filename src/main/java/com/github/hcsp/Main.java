8e99057417dbb2836f03b172267d3d804ce8f110
lyra <lyraaaaaaaaaaa@gmail.com>
2025-07-07 21:18:51 +1000

    fixed

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,13 @@
 package com.github.hcsp;
 
+
+// Fix the compilation error
+// Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
+// 修复编译错误
+// 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
+
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(relation(1, 2));
@@ -7,19 +15,17 @@ public class Main {
         System.out.println(relation(2, 2));
     }
 
-    // Fix the compilation error
-    // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
-    // 修复编译错误
-    // 在a<b时返回字符串"a<b"，在a>b时返回字符串"a>b"，否则返回"a=b"
+
     public static String relation(int a, int b) {
         if (a > b) {
             String sign = ">";
+            return "" + a + sign + b;
         } else if (a < b) {
             String sign = "<";
+            return "" + a + sign + b;
         } else {
             String sign = "=";
+            return "" + a + sign + b;
         }
-
-        return "" + a + sign + b;
     }
 }
